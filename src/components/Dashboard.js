5c39a2696715dbe048ead77242ffe52a2e941ae6
genialtechie <thehaleembello@gmail.com>
2022-09-28 12:36:28 -0400

    added delete functionality and persisted data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactComponent as List } from '../list.svg';
 import { ReactComponent as Right } from '../right.svg';
 import { ReactComponent as New } from '../new.svg';
@@ -8,9 +8,16 @@ import NewReminder from './NewReminder';
 
 const Dashboard = () => {
   const [newListBtn, setNewListBtn] = useState(false);
-  const [data, setData] = useState({ lists: [] });
+  const [data, setData] = useState(() => {
+    const lists = JSON.parse(localStorage.getItem('lists')) || [];
+    return { lists: lists };
+  });
   const [openList, setOpenList] = useState({ open: false, title: '' });
   const [newReminder, setNewReminder] = useState({ open: false, title: '' });
+
+  useEffect(() => {
+    localStorage.setItem('lists', JSON.stringify(data.lists));
+  }, [data]);
 
   function handleNewList(value) {
     if (value.length > 0) {
@@ -43,18 +50,56 @@ const Dashboard = () => {
     id === -1
       ? console.log('list not found')
       : tempList[id].reminders.push(newReminder);
-    setData((current) => {
-      return { lists: [...tempList] };
-    });
+    setData({ lists: [...tempList] });
+  }
+
+  function checkCompleted(value, list) {
+    const tempList = [...data.lists];
+    const listId = tempList.findIndex((obj) => obj.title === list);
+    const remId = tempList[listId].reminders.findIndex(
+      (obj) => obj.title === value
+    );
+    return data.lists[listId].reminders[remId].completed;
+  }
+
+  function setCompleted(value, list) {
+    const tempList = [...data.lists];
+    const listId = tempList.findIndex((obj) => obj.title === list);
+    const remId = tempList[listId].reminders.findIndex(
+      (obj) => obj.title === value
+    );
+    if (!data.lists[listId].reminders[remId].completed) {
+      tempList[listId].reminders[remId].completed = true;
+      setData({ lists: [...tempList] });
+    } else {
+      tempList[listId].reminders[remId].completed = false;
+      setData({ lists: [...tempList] });
+    }
+    return data.lists[listId].reminders[remId].completed;
+  }
+
+  function handleDelete(value) {
+    const tempList = [...data.lists];
+    const listId = tempList.findIndex((obj) => obj.title === value.list);
+    if (value.type === 'reminder' && listId !== -1) {
+      tempList[listId].reminders = tempList[listId].reminders.filter(
+        (item) => item.title !== value.title
+      );
+      setData({ lists: [...tempList] });
+    } else if (value.type === 'list' && listId !== -1) {
+      const newList = tempList.filter((item) => item.title !== value.list);
+      setOpenList({ open: false, title: '' });
+      setData({ lists: [...newList] });
+    }
   }
 
   return (
     <div className="p-5 h-full flex justify-center">
       <div className="w-full relative flex flex-col justify-between lg:w-1/3">
         <div>
-          <h1 className="mb-6 text-5xl font-bold">Reminders</h1>
+          <h1 className="mb-6 text-5xl font-bold">To-dos</h1>
           <div className="mx-auto bg-gray-100 p-8 mb-6 text-2xl rounded-lg hover:cursor-pointer transition duration-500 hover:bg-gray-200 ">
-            All
+            All To-dos
           </div>
           <div className="h-fit mb-6">
             <h1 className="mb-4 font-bold text-3xl">My Lists</h1>
@@ -80,7 +125,7 @@ const Dashboard = () => {
             className="float-left rounded-lg text-center pr-3 bg-gray-50 transition duration-500 hover:bg-gray-200 disabled:bg-gray-50"
           >
             <New className="scale-50 inline-block" />
-            <span>New Reminder</span>
+            <span>New To-do</span>
           </button>
           <button
             onClick={() => setNewListBtn(true)}
@@ -106,6 +151,9 @@ const Dashboard = () => {
           show={openList}
           getReminders={handleListPage}
           newReminder={setNewReminder}
+          deleteItem={handleDelete}
+          checked={checkCompleted}
+          setChecked={setCompleted}
         />
       </div>
     </div>
--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { ReactComponent as Close } from '../close.svg';
 import { ReactComponent as New } from '../new.svg';
+import { ReactComponent as Delete } from '../delete.svg';
 
-const ListPage = ({ close, getReminders, show, newReminder }) => {
+const ListPage = ({
+  close,
+  getReminders,
+  show,
+  newReminder,
+  deleteItem,
+  checked,
+  setChecked,
+}) => {
   const reminders = getReminders(show.title);
+
   return (
     <>
       {show.open ? (
@@ -23,12 +33,29 @@ const ListPage = ({ close, getReminders, show, newReminder }) => {
                   <input
                     type="checkbox"
                     className="scale-125"
+                    onChange={(e) => {
+                      setChecked(reminder.title, show.title);
+                      const obj = {
+                        title: reminder.title,
+                        type: 'reminder',
+                        list: show.title,
+                      };
+                      setTimeout(() => {
+                        checked(reminder.title, show.title)
+                          ? deleteItem(obj)
+                          : console.log('item unchecked');
+                      }, 1500);
+                    }}
                     name={reminder.title}
                     id={reminder.title}
                   />
                   <label
                     htmlFor={reminder.title}
-                    className="ml-4 text-lg"
+                    className={`ml-4 text-lg ${
+                      checked(reminder.title, show.title)
+                        ? 'text-gray-400 line-through'
+                        : 'no-underline text-black'
+                    }`}
                   >
                     {reminder.title}
                   </label>
@@ -39,13 +66,29 @@ const ListPage = ({ close, getReminders, show, newReminder }) => {
               </div>
             ))}
           </div>
-          <button
-            onClick={() => newReminder({ open: true, title: show.title })}
-            className="w-fit flex items-center mt-8 text-gray-400 hover:text-gray-600 hover:cursor-pointer"
-          >
-            <New className="inline-block scale-50" />
-            <span className="font-bold">New Reminder</span>
-          </button>
+          <div className="w-full flex flex-row justify-between mt-6">
+            <button
+              onClick={() => newReminder({ open: true, title: show.title })}
+              className="w-fit flex items-center text-gray-400 hover:text-gray-600 hover:cursor-pointer"
+            >
+              <New className="inline-block scale-50" />
+              <span className="font-bold">New To-do</span>
+            </button>
+            <button
+              onClick={() => {
+                const obj = {
+                  title: show.title,
+                  type: 'list',
+                  list: show.title,
+                };
+                deleteItem(obj);
+              }}
+              className="w-fit flex items-center text-gray-400 hover:text-gray-600 hover:cursor-pointer"
+            >
+              <Delete className="inline-block scale-50" />
+              <span className="font-bold">Delete list</span>
+            </button>
+          </div>
         </div>
       ) : null}
     </>
